e3f085006a56fb3ea155ce6da128638364e98c49
Blaise <153961581+MrCrack1@users.noreply.github.com>
2024-10-03 22:21:13 +0200

    Update Creating Tables.sql

--- Creating Tables.sql
+++ Creating Tables.sql
@@ -1,6 +1,6 @@
 
 # CREATING TABLES
-```    sql
+
 CREATE TABLE customers (
     customer_id NUMBER PRIMARY KEY,
     customer_name VARCHAR2(100),
@@ -9,20 +9,18 @@ CREATE TABLE customers (
     state VARCHAR2(100),
     zip VARCHAR2(20)
 );
-```
-```sql
+
 CREATE TABLE orders (
     order_id NUMBER PRIMARY KEY,
     customer_id NUMBER REFERENCES customers(customer_id),
     order_date DATE,
     order_status VARCHAR2(50)
 );
-```
-```sql
+
 CREATE TABLE products (
     product_id NUMBER PRIMARY KEY,
     product_name VARCHAR2(100),
     price NUMBER,
     quantity NUMBER
 );
-```
+
